6a47153cfb2a61cd989229bcad3489b6a1ee43da
Tom Pike <tpike3@gmu.edu>
2024-09-15 20:45:49 -0400

    Model update
    - adjust proportion array
    - results in heavy tailed distro
    - adjust CCDF which currently plots half-log

--- run.py
+++ run.py
@@ -7,7 +7,14 @@ Created on Tue Sep 10 06:43:47 2024
 
 from model import WealthModel
 
-model = WealthModel(100)
+model = WealthModel(2000)
 
-model.step()
+
+for step in range(10):
+    model.step()
+    print(step)
+    
+output = model.datacollector.get_agent_vars_dataframe()
+
+output.to_csv("inequality_output.csv")
 
