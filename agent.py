fdabdc62a3f25ed1bd1d6351ff2574619d535c33
Tom Pike <tpike3@gmu.edu>
2024-09-15 20:45:49 -0400

    Model update
    - adjust proportion array
    - results in heavy tailed distro
    - adjust CCDF which currently plots half-log

--- agent.py
+++ agent.py
@@ -9,10 +9,24 @@ import mesa
 
 class WealthAgent(mesa.Agent):
     
-    def __init__(self,model,wealth):
+    def __init__(self,model, proportion):
         super().__init__(model)
-        self.wealth=wealth
+        self.wealth=38000
+        self.W =proportion
+        
         
     def step(self):
-        print(self.wealth)
+        #increase welath by proportion - payday
+        self.wealth += (self.W*self.wealth)
+        #self.wealth -= 10000 #surival expense
+        
+        if self.wealth > 0: 
+            #get basic expenses
+            exchange_agent = self.random.choice(self.model.schedule.agents)
+            if exchange_agent is not None and exchange_agent is not self:
+                #print(self.wealth)
+                exchange_agent.wealth += (exchange_agent.W*self.wealth)
+                self.wealth -= (exchange_agent.W*self.wealth)
+                #print (exchange_agent.wealth, self.wealth) 
+                
         
--- model.py
+++ model.py
@@ -14,28 +14,30 @@ from agent import WealthAgent
 
 class WealthModel(mesa.Model): 
     
-    def __init__(self, population,
-                 average_income = 38000,
-                 standard_deviation = 1818): #obtained from https://apps.bea.gov/scb/issues/2021/01-january/0121-revisions-to-gdp-gdi.htm
+    def __init__(self, population):
+                 #average_income = 38000,
+                 #standard_deviation = 1818): #obtained from https://apps.bea.gov/scb/issues/2021/01-january/0121-revisions-to-gdp-gdi.htm
         
         super().__init__()
         self.population = population
         
         self.schedule = mesa.time.RandomActivation(self)
+        self.datacollector = mesa.DataCollector(agent_reporters={"Wealth":"wealth"})
         
         
         # create an array of iniaital weatth value    
-        gaussian_array = np.random.normal(loc=average_income,
-                                          scale=np.sqrt(2)*standard_deviation,
+        gaussian_array = np.random.normal(loc=0.5,
+                                          scale=0.15,
                                           size=self.population)
         # round array to two decimals
         gaussian_array = np.around(gaussian_array, decimals=2)
         
-        for wealth in gaussian_array:
-            agent = WealthAgent(self, wealth)
+        for idx in range(self.population):
+            agent = WealthAgent(self, gaussian_array[idx])
             self.schedule.add(agent)
     
     def step(self):
+        self.datacollector.collect(self)
         self.schedule.step()
 
            
