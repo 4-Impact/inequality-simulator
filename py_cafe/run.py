eb539355826eef1a348778be56ab7bd3770b0d61
Tom Pike <tpike3@gmu.edu>
2024-11-18 07:37:36 -0500

    PREP DOCS
    - adds basic doc structure
    - creates py.cafe folder

---
+++ py_cafe/run.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 06:43:47 2024
+
+@author: thoma
+"""
+
+from model import WealthModel
+from model import Histogram, Churn, Wealth
+from mesa.visualization import SolaraViz,make_plot_component
+
+
+model = WealthModel()
+model_params = {
+    "policy": {
+        "type": "Select",
+        "value": "innovation",
+        "values": ["econophysics", "fascist","communist", "innovation"],
+        "text": "Select Policy"
+    }
+}
+
+wealth_plot = make_plot_component("Gini")
+
+Page = SolaraViz(
+    model, 
+    components=[Churn, wealth_plot, Wealth, Histogram],
+    model_params=model_params,
+)
